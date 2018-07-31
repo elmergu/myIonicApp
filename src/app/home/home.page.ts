89bcc93e9ad8b5c2ae2a61667a001bdbaa7b8e36
Elmer Morales <Elmer.Morales@autoweb.com>
2018-07-31 16:06:30 -0600

    weather http api testing in the home page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,8 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss']
 })
-export class HomePage {}
+export class HomePage implements OnInit{
+  weather = {};
+  public now: Date = new Date();
+  
+  constructor(private http: HttpClient) { }
+  
+  getWeather() {
+    return this.http.get('http://api.openweathermap.org/data/2.5/weather?lat=14.5977959&lon=-90.5037737&units=metric&appid=')
+  }
+
+  ngOnInit() {
+    this.getWeather().subscribe(data => {
+      console.log(data);
+      this.weather = data;
+    });
+  }
+
+}
